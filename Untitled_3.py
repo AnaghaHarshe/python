72eb9c5f47bf2e5cea58fe050071a1fb864f1824
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-07-26 00:45:23 +0530

    Add files via upload

---
+++ Untitled_3.py
@@ -0,0 +1,13 @@
+a=1.3
+b=True
+c="Anagha"
+d=None
+
+print(a)
+print(b)
+a1=6.7
+print(a+a1)
+print("The type of a is ",type(a))
+print("The type of b is ",type(b))
+print("The type of c is ",type(c))
+print("The type of d is ",type(d))
