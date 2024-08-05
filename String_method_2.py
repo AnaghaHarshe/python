8be6eef732a7c363826f5b1ff72fc52e6a222476
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-08-06 00:51:49 +0530

    Add files via upload

---
+++ String_method_2.py
@@ -0,0 +1,25 @@
+a="He's name is Don. He is an honest Man."
+print(a.find("is"))  #find index of 'is' 
+
+print(a.index("Don"))  
+
+str1="Welcome Home"
+print(str1.isalnum())   #alfanumeric
+
+str2="hello world"   #use \n, \t for false value otherwise true
+print(str2.isprintable())  #print statement without any manupaulator
+
+b="hello world"
+print(b.islower())
+
+c="HELLO WORLD"
+print(c.isupper())
+
+str3="          "  #use tab key
+print(str3.isspace())
+str4="          "   #use spacekey
+print(str4.isspace())
+
+d="World Health Oganization"
+print(d.istitle()) #find title
+
