f70726973e904dde237a25b8fe89888586f68200
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-08-05 22:03:23 +0530

    Add files via upload

---
+++ String_method.py
@@ -0,0 +1,22 @@
+a="Anagha Harshe"
+b="Hello !!!!!!!!"
+print(len(a))      #lower case()
+
+print(a.upper())     #upper case()
+print(a.lower())     #lower case()
+
+print(b.rstrip("!"))   #Rstrip()
+print(a.replace("Anagha","Ankita"))  #replace()
+
+blogHeading="introduction to python"
+print(blogHeading.capitalize())   #Capitalize()
+
+c="Welcome to the console !!!"
+print(c.center(50))      #center()
+print(len(c))             #String Length
+
+d="Anagha Ankita Anagha"
+print(d.count("Anagha"))  #count
+
+e="Welcome Home !!!!"
+print(e.endswith("!!!!"))   #ends with
