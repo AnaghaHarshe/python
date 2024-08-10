15af510626362829d95d93b7ebc97b6fb740cfad
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-08-10 21:40:26 +0530

    Add files via upload

---
+++ timestamp.py
@@ -0,0 +1,9 @@
+import time
+timestamp=time.strftime('%H: %M: %S')
+print(timestamp)
+timestamp=time.strftime('%H')
+print(timestamp)
+timestmp=time.strftime('%M')
+print(timestamp)
+timestmp=time.strftime('%S')
+print(timestamp)
