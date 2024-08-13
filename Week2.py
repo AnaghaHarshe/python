844a3a8495a160ff62e793faf7bb72a99b11be86
AnaghaHarshe <159905237+AnaghaHarshe@users.noreply.github.com>
2024-08-13 19:02:25 +0530

    Add files via upload

---
+++ Week2.py
@@ -0,0 +1,23 @@
+#S={1,2,4,5}
+#l = []
+#for i in range(len(S)):
+#   l +=[l+i]
+#print(l)
+
+'''import numpy as np
+arr = np.array(np.arange(0,15))
+print(arr.reshape(3,5))'''
+
+'''place ='chennai'
+print("My friend,s house is in {}".format(place))'''
+
+'''place ='chennai'
+print("My friend's house is in {}".format(place))'''
+
+# print("My friend's house is in {}".format('chennai'))
+
+name = "Mahesh"
+l=[]
+for i in name:
+    l.append(i.capitalize())
+print(l)
