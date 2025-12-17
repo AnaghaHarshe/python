20cf9d5c94dd91756bb735ce2e00c0fb9bd7e5e2
Anagha Harshe <159905237+AnaghaHarshe@users.noreply.github.com>
2025-12-17 11:34:25 +0530

    Add dictionary example and print statement

---
+++ JS_9_12.py
@@ -0,0 +1,51 @@
+# print("Hello")
+
+# list=[11, 12, 13, 14, 15]
+# print(list)
+#
+# tuple=(11, 22, 33, 44, 55, 66, 77)
+# print(tuple)
+#
+# dict = {"Name" : "Anagha", "Age" : "22"}
+dict1 = {"Name" : "Anagha",
+         "Age" : "20",
+         "Add" : "Tilwani",}
+print(dict1)
+print((dict1), end="\n")
+#
+# set = {11, 12, 13, 14}
+
+
+# string1 = 'hello', 'bye' #' ' used for only words
+# string2 = " she is good girl"  #" " used for sentence
+# string3 = """ My name is anagha. currently pursuing my B. Tech degree at DKTE in CSE(AI)""" # """ """ used for para
+#-----------------------------------------------------------------------------------------------------------------------
+# #list
+#
+# list = [11, 12, 13, 14, 15]
+# print(len(list))  #length of list
+#
+# print(list[2])   #index no
+#
+# #built-in function min, max, sum, len
+# print(sum(list))
+# print(max(list))
+# print(min(list))
+
+#-----------------------------------------------------------------------------------------------------------------------
+# #diffrence tuple and list
+#    # list is mutable
+#    # tuple is imutable
+#
+# list2 = [11, 22, 33, 44, 55]
+# tuple2 = (11, 22, 33, 44, 55)
+#
+# list2.append(33)
+# print(list2)
+#
+# list2.extend([77])
+# print(list2)
+#
+# list3 = [11, 12, 13, 11, 15, 14, 12 ]
+# print(list(set(list3)))
+# print("the number is repeated:",list2.count(11),"times")
