34ae59d68d1de488afdc3c7eea48360d167e901a
Anagha Harshe <159905237+AnaghaHarshe@users.noreply.github.com>
2026-03-11 17:20:47 +0530

    Add files via upload

---
+++ Module_1.py
@@ -0,0 +1,15 @@
+## Modules in python is a simple file that contains python code such as functions, variables and classes.
+
+## WHY MODULES ARE USED
+## 1. To organize large program
+## 2. To reuse code in multiple program
+## 3. To avoid rewriting the same code
+## 4. To improve radiability and maintainability
+
+## Module_1.py
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+
+name = "This is first python Module Example"
---
+++ Module_1main.py
@@ -0,0 +1,15 @@
+import Module_1
+
+print(Module_1.add(10,5))
+print(Module_1.sub(10, 5))
+print(Module_1.name)
+
+
+
+## Different Ways to Import Modules
+## import math
+## print(math.sqrt(16))
+
+from math import sqrt, factorial
+print(sqrt(36))
+print(factorial(5))
